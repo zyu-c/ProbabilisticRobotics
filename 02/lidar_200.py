d01ed6b6ee84480775c8f024b54a0ab43cf398a3
ZyuC <m9t1k3zczc@gmail.com>
2020-04-03 00:19:14 +0900

    ヒストグラムに平均値出力

--- 02/lidar_200.py
+++ 02/lidar_200.py
@@ -7,9 +7,13 @@ data = pd.read_csv("../sensor_data/sensor_data_200.txt", delimiter=" ", header=N
 
 import matplotlib.pyplot as plt
 
-data["lidar"].hist(bins = max(data["lidar"]) - min(data["lidar"]), align = 'left')
+#data["lidar"].hist(bins = max(data["lidar"]) - min(data["lidar"]), align = 'left')
 #plt.show()
 
 mean1 = sum(data["lidar"].values) / len(data["lidar"].values)
 mean2 = data["lidar"].mean()
-print(mean1, mean2)
+#print(mean1, mean2)
+
+data["lidar"].hist(bins = max(data["lidar"]) - min(data["lidar"]), color = "orange", align = 'left')
+plt.vlines(mean1, ymin = 0, ymax = 5000, color = "red")
+plt.show()
