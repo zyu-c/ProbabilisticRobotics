8ae9f2abc1b364ae3e38d626db61de69abd92dc4
ZyuC <m9t1k3zczc@gmail.com>
2020-04-03 00:15:34 +0900

    ヒストグラム出力

--- 02/lidar_200.py
+++ 02/lidar_200.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
 data = pd.read_csv("../sensor_data/sensor_data_200.txt", delimiter=" ", header=None, names=("date", "time", "ir", "lidar"))
-print(data)
+#print(data)
 
-print(data["lidar"][0:5])
+#print(data["lidar"][0:5])
+
+import matplotlib.pyplot as plt
+
+data["lidar"].hist(bins = max(data["lidar"]) - min(data["lidar"]), align = 'left')
+plt.show()
