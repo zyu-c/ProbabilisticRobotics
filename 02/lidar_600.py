ab2dbe71420d7c5149f624a3d9bde765568cac13
ZyuC <m9t1k3zczc@gmail.com>
2020-04-04 00:08:15 +0900

    ベイズの定理計算

--- 02/lidar_600.py
+++ 02/lidar_600.py
@@ -44,6 +44,37 @@ p_z = pd.DataFrame(probs.transpose().sum())
 cond_z_t = probs / p_t[0]
 #print(cond_z_t)
 
-(cond_z_t[6]).plot.bar(color = "blue", alpha = 0.5)
-(cond_z_t[14]).plot.bar(color = "orange", alpha = 0.5)
+#(cond_z_t[6]).plot.bar(color = "blue", alpha = 0.5)
+#(cond_z_t[14]).plot.bar(color = "orange", alpha = 0.5)
+#plt.show()
+
+cond_t_z = probs.transpose() / probs.transpose().sum()
+#print("P(z = 630) = ", p_z[0][630])
+#print("P(t = 13) = ", p_t[0][13])
+#print("P(t = 13 | z = 630) = ", cond_t_z[630][13])
+#print("Bayes P(z = 630 | t = 13) = ", cond_t_z[630][13] * p_z[0][630] / p_t[0][13])
+#print("Answer P(z = 630 | t = 13)= ", cond_z_t[13][630])
+
+def bayes_estimation(sensor_value, curren_estimation):
+    new_estimation = []
+    for i in range(24):
+        new_estimation.append(cond_z_t[i][sensor_value] * curren_estimation[i])
+    return new_estimation / sum(new_estimation)
+
+estimation = bayes_estimation(630, p_t[0])
+#plt.plot(estimation)
+#plt.show()
+
+value_5 = [630, 632, 636]
+estimation = p_t[0]
+for v in value_5:
+    estimation = bayes_estimation(v, estimation)
+#plt.plot(estimation)
+#plt.show()
+
+value_11 = [617, 624, 619]
+estimation = p_t[0]
+for v in value_11:
+    estimation = bayes_estimation(v, estimation)
+plt.plot(estimation)
 plt.show()
