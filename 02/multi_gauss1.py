a190353d98be27875a1fb44c3398ee9cd282ab80
ZyuC <m9t1k3zczc@gmail.com>
2020-04-04 01:02:43 +0900

    二次元ガウス分布出力

--- 02/multi_gauss1.py
+++ 02/multi_gauss1.py
@@ -10,13 +10,25 @@ d = d.loc[:, ["ir", "lidar"]]
 #sns.jointplot(d["ir"], d["lidar"], d, kind = "kde")
 #plt.show()
 
-print("光センサの計測値の分散 : ", d.ir.var())
-print("LiDARの計測値の分散 : ", d.lidar.var())
+#print("光センサの計測値の分散 : ", d.ir.var())
+#print("LiDARの計測値の分散 : ", d.lidar.var())
 
 diff_ir = d.ir - d.ir.mean()
 diff_lidar = d.lidar - d.lidar.mean()
 a = diff_ir * diff_lidar
-print("共分散 : ", sum(a) / (len(d) - 1))
-print(d.mean())
+#print("共分散 : ", sum(a) / (len(d) - 1))
+#print(d.mean())
 
-print(d.cov())
+#print(d.cov())
+
+from scipy.stats import multivariate_normal
+irlidar = multivariate_normal(mean = d.mean().values.T, cov = d.cov().values)
+
+import numpy as np
+x, y = np.mgrid[0:40, 710:750]
+pos = np.empty(x.shape + (2, ))
+pos[:, :, 0] = x
+pos[:, :, 1] = y
+cont = plt.contour(x, y, irlidar.pdf(pos))
+cont.clabel(fmt = '%1.1e')
+plt.show()
