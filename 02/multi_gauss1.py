55a79323198e01e90e91de8c1e04debaaa6a57b6
ZyuC <m9t1k3zczc@gmail.com>
2020-04-04 00:45:38 +0900

    共分散行列計算

--- 02/multi_gauss1.py
+++ 02/multi_gauss1.py
@@ -7,5 +7,16 @@ data = pd.read_csv("../sensor_data/sensor_data_700.txt", delimiter=" ", header=N
 d = data[(data["time"] < 160000) & (data["time"] >= 120000)]
 d = d.loc[:, ["ir", "lidar"]]
 
-sns.jointplot(d["ir"], d["lidar"], d, kind = "kde")
-plt.show()
+#sns.jointplot(d["ir"], d["lidar"], d, kind = "kde")
+#plt.show()
+
+print("光センサの計測値の分散 : ", d.ir.var())
+print("LiDARの計測値の分散 : ", d.lidar.var())
+
+diff_ir = d.ir - d.ir.mean()
+diff_lidar = d.lidar - d.lidar.mean()
+a = diff_ir * diff_lidar
+print("共分散 : ", sum(a) / (len(d) - 1))
+print(d.mean())
+
+print(d.cov())
