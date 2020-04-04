489852f97c490b381efd11eb63327e4757848311
ZyuC <m9t1k3zczc@gmail.com>
2020-04-05 01:43:42 +0900

    ロボット移動アニメーション実装

--- 03/ideal_robot5.py
+++ 03/ideal_robot5.py
@@ -27,7 +27,7 @@ class World:
             for i in range(1000):
                 self.one_step(i, elems, ax)
         else:
-            self.ani = anm.FuncAnimation(fig, self.one_step, fargs = (elems, ax), frames = 10, interval = 1000, repeat = False)
+            self.ani = anm.FuncAnimation(fig, self.one_step, fargs = (elems, ax), frames = 100, interval = 500, repeat = False)
             plt.show()
 
     def one_step(self, i, elems, ax):
@@ -35,12 +35,15 @@ class World:
         elems.append(ax.text(-4.4, 4.5, "t = " + str(i), fontsize = 10))
         for obj in self.objects:
             obj.draw(ax, elems)
+            if hasattr(obj, "one_step"): obj.one_step(1.0)
 
 class IdealRobot:
-    def __init__(self, pose, color = "black"):
+    def __init__(self, pose, agent = None, color = "black"):
         self.pose = pose
         self.r = 0.2
         self.color = color
+        self.agent = agent
+        self.poses = [pose]
 
     def draw(self, ax, elems):
         x, y, theta = self.pose
@@ -49,6 +52,9 @@ class IdealRobot:
         elems += ax.plot([x, xn], [y, yn], color = self.color)
         c = patches.Circle(xy = (x,y), radius = self.r, fill = False, color = self.color)
         elems.append(ax.add_patch(c))
+
+        self.poses.append(self.pose)
+        elems += ax.plot([e[0] for e in self.poses], [e[1] for e in self.poses], linewidth = 0.5, color = "black")
 
     @classmethod
     def state_transition(cls, nu, omega, time, pose):
@@ -60,6 +66,31 @@ class IdealRobot:
                                     nu / omega * (-math.cos(t0 + omega * time) + math.cos(t0)),
                                     omega * time])
 
-print(IdealRobot.state_transition(0.1, 0.0, 1.0, np.array([0, 0, 0]).T))
-print(IdealRobot.state_transition(0.1, 10 / 180 * math.pi, 9.0, np.array([0, 0, 0]).T))
-print(IdealRobot.state_transition(0.1, 10 / 180 * math.pi, 18.0, np.array([0, 0, 0]).T))
+    def one_step(self, time_interval):
+        if not self.agent:
+            return
+        nu, omega = self.agent.decision()
+        self.pose = self.state_transition(nu, omega, time_interval, self.pose)
+
+class Agent:
+    def __init__(self, nu, omega):
+        self.nu = nu
+        self.omega = omega
+    
+    def decision(self, observation = None):
+        return self.nu, self.omega
+
+#print(IdealRobot.state_transition(0.1, 0.0, 1.0, np.array([0, 0, 0]).T))
+#print(IdealRobot.state_transition(0.1, 10 / 180 * math.pi, 9.0, np.array([0, 0, 0]).T))
+#print(IdealRobot.state_transition(0.1, 10 / 180 * math.pi, 18.0, np.array([0, 0, 0]).T))
+
+world = World()
+straight = Agent(0.2, 0.0)
+circling = Agent(0.2, 10.0 / 180 * math.pi)
+robot1 = IdealRobot(np.array([2, 3, math.pi / 6]).T, straight)
+robot2 = IdealRobot(np.array([-2, -1, math.pi / 5 * 6]).T, circling, "red")
+robot3 = IdealRobot(np.array([0, 0, 0]).T, color = "blue")
+world.append(robot1)
+world.append(robot2)
+world.append(robot3)
+world.draw()
