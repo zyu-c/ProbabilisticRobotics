bf142d8ed9685acddeed4c3c97512a7ec683f621
ZyuC <m9t1k3zczc@gmail.com>
2020-04-11 02:19:54 +0900

    パーティクルの実装

--- 05/mcl.py
+++ 05/mcl.py
@@ -4,11 +4,18 @@ from robot import *
 
 class Particle:
     def __init__(self, init_pose):
-        self.particle = init_pose
+        self.pose = init_pose
 
 class Mcl:
     def __init__(self, init_pose, num):
         self.particles = [Particle(init_pose) for i in range(num)]
+
+    def draw(self, ax, elems):
+        xs = [p.pose[0] for p in self.particles]
+        ys = [p.pose[1] for p in self.particles]
+        vxs = [math.cos(p.pose[2]) for p in self.particles]
+        vys = [math.sin(p.pose[2]) for p in self.particles]
+        elems.append(ax.quiver(xs, ys, vxs, vys, color = "blue", alpha = 0.5))
 
 class EstimationAgent(Agent):
     def __init__(self, nu, omega, estimetor):
@@ -16,7 +23,7 @@ class EstimationAgent(Agent):
         self.estimator = estimator
 
     def draw(self, ax, elems):
-        elems.append(ax.text(0, 0, "hoge", fontsize = 10))
+        self.estimator.draw(ax, elems)
 
 world = World(30, 0.1)
 
