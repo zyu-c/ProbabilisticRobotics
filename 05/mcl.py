5be87bb3b937bf40a4eb43b570ebdaedf95692b0
ZyuC <m9t1k3zczc@gmail.com>
2020-04-11 02:06:40 +0900

    Mclクラス追加

--- 05/mcl.py
+++ 05/mcl.py
@@ -2,8 +2,16 @@ import sys
 sys.path.append("../scripts")
 from robot import *
 
+class Particle:
+    def __init__(self, init_pose):
+        self.particle = init_pose
+
+class Mcl:
+    def __init__(self, init_pose, num):
+        self.particles = [Particle(init_pose) for i in range(num)]
+
 class EstimationAgent(Agent):
-    def __init__(self, nu, omega):
+    def __init__(self, nu, omega, estimetor):
         super().__init__(nu, omega)
 
     def draw(self, ax, elems):
@@ -17,7 +25,8 @@ for ln in [(-4, 2), (2, -3), (3, 3)]:
 world.append(m)
 
 initial_pose = np.array([2, 2, math.pi / 6]).T
-circling = EstimationAgent(0.2, 10.0 / 180 * math.pi)
+estimator = Mcl(initial_pose, 100)
+circling = EstimationAgent(0.2, 10.0 / 180 * math.pi, estimator)
 r = Robot(initial_pose, sensor = Camera(m), agent = circling)
 world.append(r)
 world.draw()
