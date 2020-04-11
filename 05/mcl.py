6b79180122628b731af70fa4f5ebb2abefc84605
ZyuC <m9t1k3zczc@gmail.com>
2020-04-12 01:27:34 +0900

    パーティクルの更新を実装

--- 05/mcl.py
+++ 05/mcl.py
@@ -7,6 +7,12 @@ class Particle:
     def __init__(self, init_pose):
         self.pose = init_pose
 
+    def motion_update(self, nu, omega, time, noise_rate_pdf):
+        ns = noise_rate_pdf.rvs()
+        noised_nu = nu + ns[0] * math.sqrt(abs(nu) / time) + ns[1] * math.sqrt(abs(omega) / time)
+        noised_omega = omega + ns[2] * math.sqrt(abs(nu) / time) + ns[3] * math.sqrt(abs(omega) / time)
+        self.pose = IdealRobot.state_transition(noised_nu, noised_omega, time, self.pose)
+
 class Mcl:
     def __init__(self, init_pose, num, motion_noise_stds):
         self.particles = [Particle(init_pose) for i in range(num)]
@@ -15,7 +21,8 @@ class Mcl:
         self.motion_noise_rate_pdf = multivariate_normal(cov = c)
 
     def motion_update(self, nu, omega, time):
-        print(self.motion_noise_rate_pdf.cov)
+        for p in self.particles:
+            p.motion_update(nu, omega, time, self.motion_noise_rate_pdf)
 
     def draw(self, ax, elems):
         xs = [p.pose[0] for p in self.particles]
@@ -25,20 +32,42 @@ class Mcl:
         elems.append(ax.quiver(xs, ys, vxs, vys, color = "blue", alpha = 0.5))
 
 class EstimationAgent(Agent):
-    def __init__(self, time_interval, nu, omega, estimetor):
+    def __init__(self, time_interval, nu, omega, estimator):
         super().__init__(nu, omega)
         self.estimator = estimator
         self.time_interval = time_interval
+        self.prev_nu = 0.0
+        self.prev_omega = 0.0
+
+    def decision(self, observation = None):
+        self.estimator.motion_update(self.prev_nu, self.prev_omega, self.time_interval)
+        self.prev_nu, self.prev_omega = self.nu, self.omega
+        return self.nu, self.omega
 
     def draw(self, ax, elems):
         self.estimator.draw(ax, elems)
 
+def trial(motion_noise_stds):
+    time_interval = 0.1
+    world = World(30, time_interval)
+    initial_pose = np.array([0, 0, 0]).T
+    estimator = Mcl(initial_pose, 100, motion_noise_stds)
+    circling = EstimationAgent(time_interval, 0.2, 10.0 / 180 * math.pi, estimator)
+    r = Robot(initial_pose, sensor = None, agent = circling, color = "red")
+    world.append(r)
+    world.draw()
+
+trial({"nn":0.01, "no":0.02, "on":0.03, "oo":0.04})
+
+'''
 initial_pose = np.array([0, 0, 0]).T
 estimator = Mcl(initial_pose, 100, motion_noise_stds = {"nn":0.01, "no":0.02, "on":0.03, "oo":0.04})
 a = EstimationAgent(0.1, 0.2, 10.0 / 180 * math.pi, estimator)
 estimator.motion_update(0.2, 10.0 / 180 * math.pi, 0.1)
 
-print(np.diag([1, 2]))
+for p in estimator.particles:
+    print(p.pose)
+'''
 
 '''
 world = World(30, 0.1)
