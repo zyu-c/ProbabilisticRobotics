1aa85e199e65d8acc6a369ac03592f5532bd8c7d
ZyuC <m9t1k3zczc@gmail.com>
2020-04-12 02:50:01 +0900

    分散をパーティクルに適用して動作確認

--- 05/mcl.py
+++ 05/mcl.py
@@ -57,30 +57,4 @@ def trial(motion_noise_stds):
     world.append(r)
     world.draw()
 
-trial({"nn":0.001, "no":0.001, "on":0.13, "oo":0.001})
-
-'''
-initial_pose = np.array([0, 0, 0]).T
-estimator = Mcl(initial_pose, 100, motion_noise_stds = {"nn":0.01, "no":0.02, "on":0.03, "oo":0.04})
-a = EstimationAgent(0.1, 0.2, 10.0 / 180 * math.pi, estimator)
-estimator.motion_update(0.2, 10.0 / 180 * math.pi, 0.1)
-
-for p in estimator.particles:
-    print(p.pose)
-'''
-
-'''
-world = World(30, 0.1)
-
-m = Map()
-for ln in [(-4, 2), (2, -3), (3, 3)]:
-    m.append_landmark(Landmark(*ln))
-world.append(m)
-
-initial_pose = np.array([2, 2, math.pi / 6]).T
-estimator = Mcl(initial_pose, 100)
-circling = EstimationAgent(0.2, 10.0 / 180 * math.pi, estimator)
-r = Robot(initial_pose, sensor = Camera(m), agent = circling)
-world.append(r)
-world.draw()
-'''
+trial({"nn":0.19, "no":0.001, "on":0.13, "oo":0.2})
