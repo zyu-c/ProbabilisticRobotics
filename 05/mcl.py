8b683fc2fec36f6a842ee78756954e51f87e7b93
ZyuC <m9t1k3zczc@gmail.com>
2020-04-12 00:59:35 +0900

    Mclクラスにガウス分布を実装

--- 05/mcl.py
+++ 05/mcl.py
@@ -1,14 +1,21 @@
 import sys
 sys.path.append("../scripts")
 from robot import *
+from scipy.stats import multivariate_normal
 
 class Particle:
     def __init__(self, init_pose):
         self.pose = init_pose
 
 class Mcl:
-    def __init__(self, init_pose, num):
+    def __init__(self, init_pose, num, motion_noise_stds):
         self.particles = [Particle(init_pose) for i in range(num)]
+        v = motion_noise_stds
+        c = np.diag([v["nn"] **2, v["no"] ** 2, v["on"] ** 2, v["oo"] ** 2])
+        self.motion_noise_rate_pdf = multivariate_normal(cov = c)
+
+    def motion_update(self, nu, omega, time):
+        print(self.motion_noise_rate_pdf.cov)
 
     def draw(self, ax, elems):
         xs = [p.pose[0] for p in self.particles]
@@ -18,13 +25,22 @@ class Mcl:
         elems.append(ax.quiver(xs, ys, vxs, vys, color = "blue", alpha = 0.5))
 
 class EstimationAgent(Agent):
-    def __init__(self, nu, omega, estimetor):
+    def __init__(self, time_interval, nu, omega, estimetor):
         super().__init__(nu, omega)
         self.estimator = estimator
+        self.time_interval = time_interval
 
     def draw(self, ax, elems):
         self.estimator.draw(ax, elems)
 
+initial_pose = np.array([0, 0, 0]).T
+estimator = Mcl(initial_pose, 100, motion_noise_stds = {"nn":0.01, "no":0.02, "on":0.03, "oo":0.04})
+a = EstimationAgent(0.1, 0.2, 10.0 / 180 * math.pi, estimator)
+estimator.motion_update(0.2, 10.0 / 180 * math.pi, 0.1)
+
+print(np.diag([1, 2]))
+
+'''
 world = World(30, 0.1)
 
 m = Map()
@@ -38,3 +54,4 @@ circling = EstimationAgent(0.2, 10.0 / 180 * math.pi, estimator)
 r = Robot(initial_pose, sensor = Camera(m), agent = circling)
 world.append(r)
 world.draw()
+'''
