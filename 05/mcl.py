2640a443d387f5d9015e260a698b2583cd321a84
ZyuC <m9t1k3zczc@gmail.com>
2020-04-15 23:25:23 +0900

    尤度関数の実装

--- 05/mcl.py
+++ 05/mcl.py
@@ -14,12 +14,25 @@ class Particle:
         noised_omega = omega + ns[2] * math.sqrt(abs(nu) / time) + ns[3] * math.sqrt(abs(omega) / time)
         self.pose = IdealRobot.state_transition(noised_nu, noised_omega, time, self.pose)
 
-    def observation_update(self, observation):
+    def observation_update(self, observation, envmap, distance_dev_rate, direction_dev):
+        for d in observation:
+            obs_pos = d[0]
+            obs_id = d[1]
+            pos_on_map = envmap.landmarks[obs_id].pos
+            particle_suggest_pos = IdealCamera.observation_function(self.pose, pos_on_map)
+            distance_dev = distance_dev_rate * particle_suggest_pos[0]
+            cov = np.diag(np.array([distance_dev ** 2, direction_dev ** 2]))
+            self.weight *= multivariate_normal(mean = particle_suggest_pos, cov = cov).pdf(obs_pos)
+
         print(observation)
 
 class Mcl:
-    def __init__(self, init_pose, num, motion_noise_stds = {"nn":0.19, "no":0.001, "on":0.13, "oo":0.2}):
+    def __init__(self, envmap, init_pose, num, motion_noise_stds = {"nn":0.19, "no":0.001, "on":0.13, "oo":0.2}, \
+            distance_dev_rate = 0.14, direction_dev = 0.05):
         self.particles = [Particle(init_pose, 1.0 / num) for i in range(num)]
+        self.map = envmap
+        self.distance_dev_rate = distance_dev_rate
+        self.direction_dev = direction_dev
         v = motion_noise_stds
         c = np.diag([v["nn"] **2, v["no"] ** 2, v["on"] ** 2, v["oo"] ** 2])
         self.motion_noise_rate_pdf = multivariate_normal(cov = c)
@@ -30,14 +43,15 @@ class Mcl:
 
     def observation_update(self, observation):
         for p in self.particles:
-            p.observation_update(observation)
+            p.observation_update(observation, self.map, self.distance_dev_rate, self.direction_dev)
 
     def draw(self, ax, elems):
         xs = [p.pose[0] for p in self.particles]
         ys = [p.pose[1] for p in self.particles]
-        vxs = [math.cos(p.pose[2]) for p in self.particles]
-        vys = [math.sin(p.pose[2]) for p in self.particles]
-        elems.append(ax.quiver(xs, ys, vxs, vys, color = "blue", alpha = 0.5))
+        vxs = [math.cos(p.pose[2]) * p.weight * len(self.particles) for p in self.particles]
+        vys = [math.sin(p.pose[2]) * p.weight * len(self.particles) for p in self.particles]
+        elems.append(ax.quiver(xs, ys, vxs, vys, \
+            angles = 'xy', scale_units = 'xy', scale = 1.5, color = "blue", alpha = 0.5))
 
 class EstimationAgent(Agent):
     def __init__(self, time_interval, nu, omega, estimator):
@@ -64,7 +78,7 @@ def trial():
         m.append_landmark(Landmark(*ln))
     world.append(m)
     initial_pose = np.array([0, 0, 0]).T
-    estimator = Mcl(initial_pose, 100)
+    estimator = Mcl(m, initial_pose, 100)
     a = EstimationAgent(time_interval, 0.2, 10.0 / 180 * math.pi, estimator)
     r = Robot(initial_pose, sensor = Camera(m), agent = a, color = "red")
     world.append(r)
